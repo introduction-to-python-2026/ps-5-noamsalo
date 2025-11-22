2cb3795376a6ba07a69e8e9081f89c87ca06500a
noamsalo <noamsal30@gmail.com>
2025-11-22 16:03:45 +0200

    Fix import statements in main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Add the import statements for functions from string_utils.py and eq
-import string_utils.py
-import equation_utils.py
-from string_utils.py import parse_chemical_reaction, count_atoms_in_reaction
-from equation_utils.py import build_equations, my_solve
+import string_utils
+import equation_utils
+from string_utils import parse_chemical_reaction, count_atoms_in_reaction
+from equation_utils import build_equations, my_solve
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
     # 1.parse reaction
