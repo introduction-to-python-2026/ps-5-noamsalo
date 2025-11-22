6f06ce39dcc271120f958750d91b71937ad77405
noamsalo <noamsal30@gmail.com>
2025-11-22 16:02:18 +0200

    Update equation_utils.py

--- equation_utils.py
+++ equation_utils.py
@@ -1,6 +1,6 @@
 # Add the import statements for necessary sympy functions here
-
-
+import sympy
+from sympy import solve, enumerate, generate_equation_for_element
 ELEMENTS = [
     'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
     'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca',
