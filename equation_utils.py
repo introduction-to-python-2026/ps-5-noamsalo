661f1605529dc4f025d67b28dfb28aed0a23687a
noamsalo <noamsal30@gmail.com>
2025-11-26 18:57:07 +0200

    Refactor imports in equation_utils.py

--- equation_utils.py
+++ equation_utils.py
@@ -1,6 +1,4 @@
-# Add the import statements for necessary sympy functions here
-import sympy
-from sympy import solve, enumerate, generate_equation_for_element
+from sympy import symbols, Eq, solve
 ELEMENTS = [
     'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
     'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca',
